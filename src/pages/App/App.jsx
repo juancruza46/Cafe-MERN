4f4727ca5998eddbaaa368ee2f9f8854f6943366
juancruza46 <juan.cruza46@gmail.com>
2024-02-12 20:03:08 -0500

    adds navbar components, usse link components to connect to existing routes

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,16 +4,21 @@ import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
-  const[user, setUser] = useState(null)
+  const[user, setUser] = useState({})
   return (
     <main className="App">
       { user ?
+        <>
+        <NavBar />
         <Routes>
           <Route path="/orders/new" element={<NewOrderPage />}/>
           <Route path="/orders" element={<OrderHistoryPage />}/>
         </Routes>
+        </>
+
         :
         <AuthPage />
       }
